2884bc039e1e84ddc272bb5c03304db90cbaf1d7
hernandez232 <54686582+hernandez232@users.noreply.github.com>
2019-10-15 12:53:28 -0600

    Create Ejercicio8.py

---
+++ Ejercicio8.py
@@ -0,0 +1,11 @@
+nu = int (input ("Ingrese un numero entero: "))
+i = 1
+print (" ")
+
+print ("Tabla de multiplicar del",nu,"del 1 al 10: ")
+for i in range (1, 11):
+    multi = nu*i
+    print (nu,"x",i,"=",multi," ")
+
+    
+
